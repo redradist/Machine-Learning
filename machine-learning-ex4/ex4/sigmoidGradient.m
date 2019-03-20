e871860dde203294e6fc5acf03e3e5b3604b8ef0
redradist <redradist@gmail.com>
2019-03-20 22:41:14 +0200

    Finished Sigmoid Gradient execise

--- machine-learning-ex4/ex4/sigmoidGradient.m
+++ machine-learning-ex4/ex4/sigmoidGradient.m
@@ -6,28 +6,15 @@ function g = sigmoidGradient(z)
 %   vector. In particular, if z is a vector or matrix, you should return
 %   the gradient for each element.
 
-g = zeros(size(z));
+g = zeros(size(z))
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the gradient of the sigmoid function evaluated at
 %               each value of z (z can be a matrix, vector or scalar).
 
-
-
-
-
-
-
-
-
-
-
-
-
+sigs = sigmoid(z)
+g = sigs .* (ones(size(z)) - sigs)
 
 % =============================================================
-
-
-
 
 end
